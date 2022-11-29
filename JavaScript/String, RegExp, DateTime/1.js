9c6c25f3afe7ad8a8bbb50919ba301186072670b
Keshah <keshah716@gmail.com>
2022-11-29 06:30:48 -0800

    .

---
+++ JavaScript/String, RegExp, DateTime/1.js
@@ -0,0 +1,7 @@
+// Given the string 'ahb acb aeb aeeb adcb axeb'.
+// Write a regular expression that matches the strings ahb, acb, aeb by pattern: letter 'a', any character, letter 'b
+
+const testStr = 'ahb acb aeb aeeb adcb axeb'
+const re = /a.b/gi;
+console.log(testStr.match(re))
+
---
+++ JavaScript/String, RegExp, DateTime/3.js
@@ -0,0 +1,7 @@
+// Get the day, month and year of the current date and output it to the console separately
+
+let date = new Date();
+
+console.log( date.getDay() );
+console.log( date.getMonth() );
+console.log( date.getFullYear() );
