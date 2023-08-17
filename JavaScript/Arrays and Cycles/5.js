da384cf8f929cf374f891d5ffd0c7d14847708c2
keshah716 <keshah716@gmail.com>
2023-08-17 12:04:59 +0400

    Update 5.js

--- JavaScript/Arrays and Cycles/5.js
+++ JavaScript/Arrays and Cycles/5.js
@@ -1,5 +1,5 @@
 // Sort the array [1,6,7,8,3,4,5,6] in descending order
-let arr = [1, 2, 3, 4, 5];
+let arr = [1,6,7,8,3,4,5,6];
 arr.reverse();
 
-alert( arr ); // 5,4,3,2,1
+alert( arr ); // 6,5,4,3,8,7,6,1
